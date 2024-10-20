0b4b2d45c32c0e0bcdfb15c49f6201147eb29561
Sato <167418137+yatai310@users.noreply.github.com>
2024-10-20 06:48:32 +0000

    addPage

---
+++ src/pages/CreateNews.jsx
@@ -0,0 +1,10 @@
+// こちらは、/createNews Newsの編集用ページです。
+
+import { Button } from "../components/Button";
+import { Image } from "../components/Image";
+import { useNavigate } from "react-router-dom";
+
+export default function CreateNews() {
+  const navigate = useNavigate();
+  return <div>閉店です。</div>;
+}
